e1c1a615e26e103844358b779be93abfd7871274
qz13k <qz13k@DESKTOP-BBEBGHO>
2016-05-16 15:53:46 +0800

    using control

---
+++ MVCWebsite/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebsite.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //    return "this is index";
+        //}
+
+        public string Welcome(string ID)
+        {
+            return HttpUtility.HtmlEncode("Hello " + ID);
+        }
+    }
+}
