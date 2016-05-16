b8fb33b007777d6d2bb405ea97235094b7120a02
qz13k <qz13k@DESKTOP-BBEBGHO>
2016-05-16 15:11:38 +0800

    添加项目文件。

---
+++ MVCWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWebsite.MvcApplication" Language="C#" %>
---
+++ MVCWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCWebsite.Startup))]
+namespace MVCWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
